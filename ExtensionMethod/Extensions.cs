1d8c57ce2dad7e3d6eb2d2503d6876ff1a5fe06b
jprakashleo <jprakash.leo@gmail.com>
2022-06-26 01:53:25 +0530

    Add project files.

---
+++ ExtensionMethod/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+
+namespace ExtensionMethod
+{
+    internal static class Extensions
+    {
+
+        public static string ToProperCase(this string input)
+        {
+            if (input.Length > 0)
+            {
+                return input.Substring(0, 1).ToUpper() + input.Substring(1).ToLower();
+            }
+            return "";
+        }
+
+        //example of generic + extension
+        //generic List to datatable.
+        public static DataTable ToDatatable<T>(this List<T> input)
+        {
+            DataTable dt = new DataTable();
+            //reading properties of class
+            PropertyDescriptorCollection propertyDescriptor = TypeDescriptor.GetProperties(typeof(T));
+            //adding each peroperty into dt column
+            foreach (PropertyDescriptor property in propertyDescriptor)
+            {
+                dt.Columns.Add(property.Name, property.PropertyType);
+            }
+            //loop for each row data
+            foreach(var item in input)
+            {
+                DataRow drow = dt.NewRow();
+                foreach(PropertyDescriptor property in propertyDescriptor)
+                {
+                    drow[property.Name] = property.GetValue(item) ?? DBNull.Value;  
+                }
+                dt.Rows.Add(drow);
+            }
+            return dt;
+        } 
+    }
+}
---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,51 @@
+﻿using ExtensionMethod;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("JAIPRkash".ToProperCase());
+Console.WriteLine("hardik punjabi".ToProperCase());
+
+List<CityStates> RTOlist = new List<CityStates>();
+
+CityStates citys = new CityStates();
+
+citys.Id = 14;
+citys.City = "Jaipur";
+citys.State = "Rajasthan";
+RTOlist.Add(citys);
+
+CityStates citys1 = new CityStates();
+citys1.Id = 13;
+citys1.City = "Ganganagar";
+citys1.State = "Rajasthan";
+RTOlist.Add(citys1);
+
+CityStates citys2 = new CityStates();
+citys2.Id = 19;
+citys2.City = "Jodhpur";
+citys2.State = "Rajasthan";
+RTOlist.Add(citys2);
+
+CityStates citys3 = new CityStates();
+citys3.Id = 15;
+citys3.City = "Jaiselmer";
+citys3.State = "Rajasthan";
+RTOlist.Add(citys3);
+
+Console.WriteLine(RTOlist[0].City + "  " + RTOlist[1].City + "  " + RTOlist[2].City);
+
+var d = RTOlist.ToDatatable();
+
+for(int i = 0; i < d.Rows.Count; i++)
+{
+    Console.WriteLine(d.Rows[i]["id"].ToString() + "  " + d.Rows[i]["city"].ToString() +" " + d.Rows[i]["state"].ToString());
+}
+Console.ReadKey();
+public class CityStates
+{
+    public int Id { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+
+}
